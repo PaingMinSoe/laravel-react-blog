18d30fca7e38eb8e49f957931089b1d2af1c7ccf
Paing Min Soe <=>
2024-04-18 11:35:32 -0700

    remove using ThemeContext for now

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.jsx'
-import { ThemeContextProvider } from './contexts/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThemeContextProvider>
     <RouterProvider router={router} />
-  </ThemeContextProvider>
 )
