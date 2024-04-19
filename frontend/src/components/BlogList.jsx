105be1f3ada285928fa5482d6c786034ce411cf2
Paing Min Soe <=>
2024-04-19 16:27:16 -0700

    move params inside the useeffect for search

--- frontend/src/components/BlogList.jsx
+++ frontend/src/components/BlogList.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import DummyImage from '../assets/aang-redirects-lightning.jpeg'
 import { Link, useLocation } from 'react-router-dom'
 import axios from "axios";
+import { CSSTransition } from 'react-transition-group';
 
 export default function BlogList({homepage, filters}) {
 
@@ -9,12 +10,15 @@ export default function BlogList({homepage, filters}) {
     const [loading, setLoading] = useState(false);
 
     const location = useLocation();
-    const params = new URLSearchParams(location.search);
 
     useEffect(() => {
+        setBlogs([]);
         setLoading(true);
         let url = 'http://localhost:8000/api/blogs';
-        let search = params.get('search');
+
+        const params = new URLSearchParams(location.search);
+        const search = params.get('search');
+
         if (homepage) {
             url += '?limit=3';
         }
@@ -26,7 +30,6 @@ export default function BlogList({homepage, filters}) {
         if (filters && filters.length > 0) {
             url += search ? '&' : '?';
             filters.map((filter, index) => {
-
                 url += `categories[]=${filter}${(index < filters.length - 1) ? '&' : ''}`;
             });
         }
@@ -39,31 +42,26 @@ export default function BlogList({homepage, filters}) {
         .catch((err) => {
             console.error(err);
         })
-    }, [homepage, setBlogs, filters]);
+    }, [homepage, setBlogs, filters, location]);
 
     return (
         <>
-            {
-                loading && `Loading...`
-            }
-            {!loading && blogs.length > 1 ? <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 p-3">
-                {
-                    blogs.map((item) => (
-                        <Link className='flex flex-col border border-gray-700 m-5 md:m-0 rounded-lg shadow-lg overflow-hidden' key={item.id}>
-                            <img src={DummyImage} alt="" />
-                            <div className='p-2.5 flex-grow'>
-                                <h1 className='text-xl font-semibold'>{item.title}</h1>
-                                <p>
-                                    {`${item.body.slice(0,180)} ${item.body.length > 180 ? "..." : ""}`}
-                                </p>
-                            </div>
-                            <div className='mx-2.5 mb-3'>
-                                <button className='px-4 py-1.5 rounded-lg bg-blue-200 hover:bg-blue-400 dark:bg-blue-600 dark:hover:bg-blue-900 transition-all ease-in'>Read More</button>
-                            </div>
-                        </Link>
-                    ))
-                }
-            </div> : <h1 className='text-3xl font-bold'>Nothing to show...</h1>}
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 p-3">
+                {blogs.map((item) => (
+                    <Link className='flex flex-col border border-gray-700 m-5 md:m-0 rounded-lg shadow-lg overflow-hidden' key={item.id}>
+                        <img src={DummyImage} alt="" />
+                        <div className='p-2.5 flex-grow'>
+                            <h1 className='text-xl font-semibold'>{item.title}</h1>
+                            <p>
+                                {`${item.body.slice(0,180)} ${item.body.length > 180 ? "..." : ""}`}
+                            </p>
+                        </div>
+                        <div className='mx-2.5 mb-3'>
+                            <button className='px-4 py-1.5 rounded-lg bg-blue-200 hover:bg-blue-400 dark:bg-blue-600 dark:hover:bg-blue-900 transition-all ease-in'>Read More</button>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </>
     )
 }
