deaaf71dcf81e6442ad415c8863102580d429d5d
Paing Min Soe <=>
2024-04-18 17:36:25 -0700

    Revert "remove using ThemeContext for now"
    This reverts commit 18d30fca7e38eb8e49f957931089b1d2af1c7ccf.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.jsx'
+import { ThemeContextProvider } from './contexts/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeContextProvider>
     <RouterProvider router={router} />
+  </ThemeContextProvider>
 )
