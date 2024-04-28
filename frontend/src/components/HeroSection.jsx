8da655dd07d814c2eb4f0ae0012eb8ebfeff2aa6
Paing Min Soe <=>
2024-04-27 17:34:55 -0700

    restyled hero section

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
   return (
     <div className='grid md:grid-cols-2'>
         <div className='flex items-center justify-end order-2 md:order-1'>
-            <div className='text-start p-4 md:p-0 space-y-4 md:w-[60%]'>
+            <div className='text-start p-4 md:p-0 space-y-4 xl:w-[60%]'>
                 <h1 className='font-bold text-6xl md:text-7xl'>Welcome to My Blog!</h1>
                 <p>I am gonne be blogging my experince as an avatar!</p>
             </div>
